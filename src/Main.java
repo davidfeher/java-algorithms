e6aee4849c94bf682e50214337b6880722b8ae6f
JacketWhite <feherkeee@gmail.com>
2017-05-15 13:29:00 +0200

    Main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
 import sorting.BubbleSort;
+import sorting.MergeSort;
+import sorting.QuickSort;
+import sorting.SelectionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = {10, 9, 4, 11, 3, 2, 1, 0, 19, 5};
+        MergeSort sorter = new MergeSort();
+        sorter.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[]{10, 9, 4, 11, 3, 2, 1, 0, 19, 5};
+        QuickSort.quickSort(array);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[]{10, 9, 4, 11, 3, 2, 1, 0, 19, 5};
         BubbleSort.Sort(array);
         System.out.println(Arrays.toString(array));
+        }
     }
-}
