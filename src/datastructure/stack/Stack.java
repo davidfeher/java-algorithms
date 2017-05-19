3f0b7c77b1fc9c51f92165015baea74d4a2a145c
JacketWhite <feherkeee@gmail.com>
2017-05-19 13:32:17 +0200

    made it generic

--- src/datastructure/stack/Stack.java
+++ src/datastructure/stack/Stack.java
@@ -3,7 +3,7 @@ package datastructure.stack;
 /**
  * Created by feher on 2017. 05. 18..
  */
-public interface Stack {
+public interface Stack<E> {
     public void push(int data);
     public boolean pop();
     public int peek();
